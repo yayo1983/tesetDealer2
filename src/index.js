34e05594a493dadc3479353883686804800886c6
yasser <yazanenator@gmail.com>
2023-02-09 16:32:06 -0600

    proyecto del frontend

--- src/index.js
+++ src/index.js
@@ -1,14 +1,53 @@
-import React from 'react';
+import React, {useRef} from "react";
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import ReportPackageList from './components/Tracking/ReportPackageList';
+import TrackingPackageList from './components/Tracking/TrackingPackageList';
+import RegisterPackage from './components/Tracking/RegisterPackage';
+import UpdatePackage from './components/Tracking/UpdatePackage';
+import First from './components/Tracking/First';
+import NavBarr from "./components/navBarr/NavBarr";
+import {BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Toast } from 'primereact/toast';
 import reportWebVitals from './reportWebVitals';
+import "bootstrap/dist/css/bootstrap.min.css"  
+import './index.css';
+//theme
+import "primereact/resources/themes/lara-light-indigo/theme.css";     
+    
+//core
+import "primereact/resources/primereact.min.css";
+
+//icons
+import "primeicons/primeicons.css"; 
+
+const {get} = require("./common/Common");
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const getUser = async () => {
+  try{
+      const { data } = await get('user');
+      return data;
+  }catch(error){
+      console.log(error.message);
+  }
+}
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <NavBarr getUser={getUser}  />
+    <div className="container-fluid my-4">
+      <Routes>  
+            <Route exact path="/" element={<First getUser={getUser}/>} />
+            <Route exact path="/package/create" element={<RegisterPackage  />} />
+            <Route exact path="/trackings" element={<TrackingPackageList  />} />
+            <Route exact path="/package/update" element={<UpdatePackage  />} />
+            <Route exact path="/package/report" element={<ReportPackageList />} />
+      </Routes>
+
+    </div>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
