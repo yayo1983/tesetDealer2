9a4b3403633cf7ee4f4e3dfacf0b7c1ee159a283
yasser <yazanenator@gmail.com>
2023-02-10 09:56:00 -0600

    poniendo configuraciones para el csrf token

--- testDealer/testDealer/settings.py
+++ testDealer/testDealer/settings.py
@@ -27,11 +27,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-CORS_ALLOWED_ORIGINS = ["http://localhost:3000", "http://localhost:8080"]
+CORS_ALLOWED_ORIGINS = ["http://127.0.0.1:3000", "http://127.0.0.1:8080", "http://localhost:3000", "http://localhost:8080"]
 
 CORS_ALLOW_CREDENTIALS = True
 
-CSRF_COOKIE_NAME = "XSRF-TOKEN"
+# CSRF_COOKIE_NAME = "XCSRF-TOKEN"
 
 # Application definition
 
