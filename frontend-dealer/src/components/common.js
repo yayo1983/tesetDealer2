163d402e64fa27d90324a689026189dedb454bc9
yasser <yazanenator@gmail.com>
2023-02-10 02:24:03 -0600

    report package terminada

--- frontend-dealer/src/components/common.js
+++ frontend-dealer/src/components/common.js
@@ -1,2 +1,20 @@
-const axios = require('axios');
+import axios from 'axios';
+
+const endPoint = "http://localhost:8000/";
+
+export const get = async (url, data=null) => {
+    return await axios.get(endPoint+url, data ? { params: data } : null);
+};
+
+export const post = async (endpoint, data) => {
+    return await axios.post(endpoint, data);
+};
+
+export const put = async (url, data) => {
+    return await axios.put(endPoint+url, data);
+};
+
+export const patch = async (url, data) => {
+    return await axios.patch(endPoint+url, data);
+};
 
--- frontend-dealer/src/components/tracking/reportPackages.js
+++ frontend-dealer/src/components/tracking/reportPackages.js
@@ -1,20 +1,71 @@
-import React, { useState } from "react";
-import { Calendar } from "primereact/calendar";
+import React, { useState, useRef } from "react";
+import { post } from "../common";
+import { Toast } from "primereact/toast";
 import { Panel } from "primereact/panel";
-import { Message } from "primereact/message";
 import { Button } from "primereact/button";
-import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
+import { Message } from "primereact/message";
+import { Calendar } from "primereact/calendar";
+import { DataTable } from "primereact/datatable";
 
 const ReportPackages = () => {
+  const toast = useRef(null);
   const [date, setDate] = useState(null);
-  const [error, setError] = useState(true);
+  const [error, setError] = useState(false);
   const [trackings, setTrackings] = useState([]);
+
+  const clearForm = (message) => {
+    setError(false);
+    showToast("success", "Success", message);
+  };
+
+  const showToast = (severity, summary, detail) => {
+    toast.current.show({
+      severity: severity,
+      summary: summary,
+      detail: detail,
+      life: 3000,
+    });
+  };
+
+  const validateCalendarInput = () => {
+    if (date === null || date === "") {
+      return false;
+    }
+    return true;
+  };
+
+  const getTrackings = async () => {
+    try {
+      if (validateCalendarInput()) {
+		setError(false);
+        let data = {
+          date: date,
+        };
+        let response = await post("package/report", data);
+        response = JSON.stringify(response);
+        if (response.status !== 200) {
+          showToast("error", "Error", "Error en la petición");
+        } else {
+          clearForm("Se ha realizado la petición exitosamente");
+          setDate(null);
+        }
+      }else{
+		setError(true);
+		showToast("error", "Error", "Debe seleccionar una fecha");
+	  }
+    } catch (error) {
+      console.log(error);
+      setTrackings([]);
+      showToast("error", "Error", "Error en la petición");
+    }
+  };
 
   let classError = error ? "p-invalid mr-2" : "";
 
   return (
     <>
+      <Toast ref={toast} />
       <div className="row">
         <div className="col-sm-4"></div>
         <div className="col-sm-4">
@@ -36,17 +87,22 @@ const ReportPackages = () => {
               tooltip="Seleccione una fecha para generar el reporte"
               readOnlyInput
               className={classError}
+              required
             />
-            <Message severity="error" text="Seleccione una fecha" />
+            {error && <Message severity="error" text="Seleccione una fecha" />}
             <div className="flex flex-wrap align-items-center justify-content-left">
               <br />
-              <Button label="Buscar" icon="pi pi-check" />
+              <Button
+                label="Buscar"
+                icon="pi pi-check"
+                onClick={getTrackings}
+              />
             </div>
           </Panel>
           <div className="col-sm-4"></div>
         </div>
       </div>
-	  <br/>
+      <br />
       <div className="row">
         <div className="col-sm-4"></div>
         <div className="col-sm-8">
@@ -55,7 +111,7 @@ const ReportPackages = () => {
             <Column field="status" header="Estado del paquete"></Column>
             <Column field="date" header="Fecha de rastreo"></Column>
             <Column field="address" header="Ubicación"></Column>
-			<Column field="type" header="Tipo de ubicación"></Column>
+            <Column field="type" header="Tipo de ubicación"></Column>
           </DataTable>
         </div>
         <div className="col-sm-4"></div>
